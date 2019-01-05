89a3b84e93587b275b50d8bd023a29386adf9a36
gost-gk <sakuva12+gh_gost@gmail.com>
2019-01-05 13:34:13 +0400

    Add: function pointers support

--- ptr_natal.py
+++ ptr_natal.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env
-import text_util as Text
+import random
 from collections import defaultdict
 
+import text_util as Text
+
 _PTR_BASE = 'указател'
+_PROC_BASE = 'подпрограмм'
 
 # Замена слов с соответствующим окончанием указателем, экспериментально
 _PTRS_SUFFIXES = {
@@ -24,12 +27,34 @@ _PTRS_PREPOSITIONS = {
     'под': 'ем',
 }
 
+# Генерация цепочек "указатель на указатель на..."
+_PTRS_CHAIN_CHANCE = 0.1  # Шанс сгенерировать цепочку после очередного слова
+_PTRS_CHAIN_LEN = (2, 5)  # Интервал длин цепочки
+_PROC_CHANCE = 0.4        # Шанс получить указатель на подпрограмму!
+
+def _natal_ptrs_chain():
+    ptrs_chain = []
+    for _ in range(random.randint(*_PTRS_CHAIN_LEN)):
+        if random.random() < _PROC_CHANCE:
+            ptrs_chain += [  # [Указатель на] подпрограмму, принимающую [указатель на...]
+                Text.PunctedWord(_PROC_BASE, 'у', ','),
+                Text.PunctedWord('принима', 'ющую', '')
+            ]
+        else:
+            ptrs_chain += [
+                Text.PunctedWord(_PTR_BASE, 'ь', ''),
+                Text.PunctedWord('на', '', '')
+            ]
+    return ptrs_chain
+
+
 def _natal_ptr_preposition(word):
     if len(word.suffix) == 0:
         if word.root in _PTRS_PREPOSITIONS:
             return Text.PunctedWord(_PTR_BASE, _PTRS_PREPOSITIONS[word.root], word.punct)
     else:
         return None
+
 
 def natal_ptr_words_list(words):
     output_words = [words[0]]
@@ -48,6 +73,13 @@ def natal_ptr_words_list(words):
         # Комбо из нескольких указателей заменяем цепочкой!
         if output_words[-1].root.lower() == _PTR_BASE and word.root.lower() == _PTR_BASE:
             output_words += [Text.PunctedWord('на', '', '')]
+
+        if random.random() < _PTRS_CHAIN_CHANCE:
+            chain = _natal_ptrs_chain()
+            if output_words[-1].is_ending_word():
+                chain[-1].punct = output_words[-1].punct
+                output_words[-1].punct = ''
+            output_words += chain
 
         output_words += [word]
 
